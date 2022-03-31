7ac754c76c6245a9f47c347858dc6c2d0945113b
Maja (they/them) <94635445+elliottm24@users.noreply.github.com>
2022-03-30 18:32:02 -0700

    Worked a bit on the AI

--- app/src/main/java/edu/up/cs301/rummikub/AI.java
+++ app/src/main/java/edu/up/cs301/rummikub/AI.java
@@ -3,10 +3,12 @@ package edu.up.cs301.rummikub;
 
 import edu.up.cs301.game.GameComputerPlayer;
 import edu.up.cs301.game.infoMsg.GameInfo;
+import edu.up.cs301.pig.PigGameState;
+import edu.up.cs301.pig.PigRollAction;
 
 /**
  * @authors Jacob Arnez, Maja Elliott, Dylan Kim, Chase Ohmstede
- * @version 3/17/2022
+ * @version 3/30/2022
  *
  * Compiles AIs
  *
@@ -16,16 +18,21 @@ import edu.up.cs301.game.infoMsg.GameInfo;
 
 public class AI extends GameComputerPlayer {
 
-    /**
-     * ctor does nothing extra
-     */
     public AI(String name) {
         super(name);
     }
 
     @Override
     protected void receiveInfo(GameInfo info) {
+        RummikubGameState recieved_State = new RummikubGameState ((RummikubGameState) info);
 
+        if(playerNum != recieved_State.getCurr_turn()){
+            return;
+        }
+        if (playerNum == recieved_State.getCurr_turn()){
+            DrawTile draw = new DrawTile(this);
+            game.sendAction(draw);
+        }
     }
     //Needs to be able to tell which AI is which
     //Updates game and makes decisions (vary between AI)
