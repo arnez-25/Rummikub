ab130dacf085169aeeab68c21dd8aba92f4e967e
ohchaseoh <Anubis7@>
2022-03-31 21:27:31 -0700

    made a 2x12 array of invisible "tile" drawings to represent hand (basically last commit)

--- app/src/main/java/edu/up/cs301/rummikub/RummiView.java
+++ app/src/main/java/edu/up/cs301/rummikub/RummiView.java
@@ -24,26 +24,26 @@ public class RummiView extends SurfaceView {
     public RummikubGameState newState;
     public Tile info;
 
-    private static final int tileColor        = Color.parseColor("#FAD3BB");
-    private static final int tileColor_invis  = Color.parseColor("#2C3493");
-    private static final int tileColor_black  = Color.parseColor("#000000");
-    private static final int tileColor_blue   = Color.parseColor("#29ABE2");
-    private static final int tileColor_red    = Color.parseColor("#ED1C24");
-    private static final int tileColor_orange = Color.parseColor("#FBB03B");
-    private static final int gridColor        = Color.parseColor("#FFFFFF");
-    private static final int trayColor        = Color.parseColor("#AE9276");
+    private static final int tileColor          = Color.parseColor("#FAD3BB");
+    private static final int boardColor_invis   = Color.parseColor("#2C3493");
+    private static final int trayColor_invis    = Color.parseColor("#AE9276");
+    private static final int gridColor          = Color.parseColor("#FFFFFF");
+    private static final int tileColor_black    = Color.parseColor("#000000");
+    private static final int tileColor_blue     = Color.parseColor("#29ABE2");
+    private static final int tileColor_red      = Color.parseColor("#ED1C24");
+    private static final int tileColor_orange   = Color.parseColor("#FBB03B");
 
-    Paint tilePaint         = new Paint();
-    Paint tilePaint_invis   = new Paint();
-    Paint tilePaint_black   = new Paint();
-    Paint tilePaint_blue    = new Paint();
-    Paint tilePaint_red     = new Paint();
-    Paint tilePaint_orange  = new Paint();
-    Paint gridPaint         = new Paint();
-    Paint trayPaint         = new Paint();
+    Paint tilePaint             = new Paint();
+    Paint boardPaint_invis      = new Paint();
+    Paint trayPaint_invis       = new Paint();
+    Paint gridPaint             = new Paint();
+    Paint tilePaint_black       = new Paint();
+    Paint tilePaint_blue        = new Paint();
+    Paint tilePaint_red         = new Paint();
+    Paint tilePaint_orange      = new Paint();
 
-    private final int rowCount      = 4;  //# of rows in grid
-    private final int colCount      = 13; //# of cols in grid
+    private float height_tile;
+    private float width_tile;
 
     public RummiView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -56,27 +56,27 @@ public class RummiView extends SurfaceView {
         tilePaint.setStyle(Paint.Style.FILL);
 
         // to turn tile color invisible, the color value will be the same as the background B)
-        tilePaint_invis.setColor(tileColor_invis);
-        tilePaint.setStyle(Paint.Style.FILL);
+        boardPaint_invis.setColor(boardColor_invis);
+        boardPaint_invis.setStyle(Paint.Style.FILL);
 
-        tilePaint_black.setColor(tileColor_black);
-        tilePaint.setStyle(Paint.Style.FILL);
-
-        tilePaint_blue.setColor(tileColor_blue);
-        tilePaint.setStyle(Paint.Style.FILL);
-
-        tilePaint_red.setColor(tileColor_red);
-        tilePaint.setStyle(Paint.Style.FILL);
-
-        tilePaint_orange.setColor(tileColor_orange);
-        tilePaint.setStyle(Paint.Style.FILL);
+        trayPaint_invis.setColor(trayColor_invis);
+        trayPaint_invis.setStyle(Paint.Style.FILL);
 
         gridPaint.setColor(gridColor);
         gridPaint.setStyle(Paint.Style.STROKE);
         gridPaint.setStrokeWidth(3);
 
-        trayPaint.setColor(trayColor);
-        trayPaint.setStyle(Paint.Style.FILL);
+        tilePaint_black.setColor(tileColor_black);
+        tilePaint_black.setStyle(Paint.Style.FILL);
+
+        tilePaint_blue.setColor(tileColor_blue);
+        tilePaint_blue.setStyle(Paint.Style.FILL);
+
+        tilePaint_red.setColor(tileColor_red);
+        tilePaint_red.setStyle(Paint.Style.FILL);
+
+        tilePaint_orange.setColor(tileColor_orange);
+        tilePaint_orange.setStyle(Paint.Style.FILL);
 
         //initializing the tiles
         for (int i = 0; i < 4; i++){
@@ -85,7 +85,6 @@ public class RummiView extends SurfaceView {
                 info.setTileNum(i);
             }
         }
-
     }
 
     @Override
@@ -118,6 +117,7 @@ public class RummiView extends SurfaceView {
 
         // vertical lines
 
+        int colCount = 13; //# of cols in grid
         for (int i = 0; i <= colCount; i++)
         {
             float pos = (width_grid / colCount) * (i);
@@ -125,6 +125,8 @@ public class RummiView extends SurfaceView {
         }
 
         // horizontal lines
+
+        int rowCount = 4; //# of rows in grid
 
         for (int i = 0; i <= rowCount; i++)
         {
@@ -137,23 +139,32 @@ public class RummiView extends SurfaceView {
          *   draw invisible tiles within each of the grid allotments.
          *   When a player wants to play a tile from their hand onto the
          *   board, we use OnTouch() hitboxes to determine which invisible
-         *   tile was selected. Then, we set the Paint style from STROKE to
-         *   FILL (thus making it visible), and reference the tile number
+         *   tile was selected. Then, we set the Paint from dark blue to
+         *   tan (thus making it visible), and reference the tile number
          *   and color from hand to create a matching drawText.
          *      - Chase
          */
 
         // invis tile placement
 
+        float pos_x1, pos_y1, pos_x2, pos_y2;
+
         for (int i = 0; i < colCount; i++) {
 
             for (int j = 0; j < rowCount; j++) {
 
-                float pos_x1 = (width_grid  / colCount) * (i);
-                float pos_y1 = (height_grid / rowCount) * (j);
-                float pos_x2 = (width_grid  / colCount) * (i + 1);
-                float pos_y2 = (height_grid / rowCount) * (j + 1);
-                c.drawRect(pos_x1 + 10, pos_y1 + 10, pos_x2 - 10, pos_y2 - 10, tilePaint_invis);
+                pos_x1 = (width_grid  / colCount) * (i);
+                pos_y1 = (height_grid / rowCount) * (j);
+                pos_x2 = (width_grid  / colCount) * (i + 1);
+                pos_y2 = (height_grid / rowCount) * (j + 1);
+                //System.out.println(pos_x2);
+                c.drawRect(pos_x1 + 10, pos_y1 + 10, pos_x2 - 10, pos_y2 - 10, boardPaint_invis);
+
+                if ((i == 0) && (j == 0)) {
+                    setWidth_tile(pos_x2);
+                    setHeight_tile(pos_y2);
+                }
+
             }
         }
     }
@@ -161,10 +172,50 @@ public class RummiView extends SurfaceView {
     /* in charge of drawing the player's hand tray for the .xml */
     public void createTray(Canvas c) {
 
-        float height_tray = (((float) getHeight() / 3) * 2) + 40;
-        float width_tray = getWidth() - 40;
-        c.drawRect(40, height_tray, width_tray, height_tray + 285, trayPaint);
+        float height_tray = (((float) getHeight() / 3) * 2) + 20;
+        float width_tray = getWidth() - 20;
 
+        c.drawRect(20, height_tray, width_tray, height_tray + height_tray, trayPaint_invis);
+
+        /**
+         * Here we're essentially doing what we did earlier within the createGrid method.
+         * This nested for-loop is making a 2x12 array of invisible "tile" drawings to be
+         * colored in earlier with onTouch() methods (represents the hand)
+         */
+
+        int colCount = 12;
+        int rowCount = 2;
+
+        float pos_x1, pos_y1, pos_x2, pos_y2;
+
+        for (int i = 0; i < colCount; i++) {
+
+            for (int j = 0; j < rowCount; j++) {
+
+                pos_x1 = (getWidth_tile() * (i))        + 60;
+                pos_y1 = (getHeight_tile() * (j))       + height_tray + 10;
+                pos_x2 = (getWidth_tile() * (i + 1))    + 60;
+                pos_y2 = (getHeight_tile() * (j + 1))   + height_tray + 10;
+
+                c.drawRect(pos_x1 + 10, pos_y1 + 10, pos_x2 - 10, pos_y2 - 10, trayPaint_invis);
+            }
+        }
+    }
+
+    public float getHeight_tile() {
+        return height_tile;
+    }
+
+    public void setHeight_tile(float height_tile) {
+        this.height_tile = height_tile;
+    }
+
+    public float getWidth_tile() {
+        return width_tile;
+    }
+
+    public void setWidth_tile(float width_tile) {
+        this.width_tile = width_tile;
     }
 
     public RummikubGameState getGameState() { return newState; }
